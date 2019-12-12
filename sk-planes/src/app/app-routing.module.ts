9b94f76234446931b9d2ce2cf651fe626d2ae9df
margie <margie@ais.pl>
2019-12-12 13:59:32 +0100

    FlightsModule i header aplikacji

--- sk-planes/src/app/app-routing.module.ts
+++ sk-planes/src/app/app-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
 import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { FlightsComponent } from './flights/flights.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent },
-];
+  {path: 'dashboard', 
+  component: DashboardComponent,
+children:[
+{  path: '', redirectTo: 'flights', pathMatch: 'full'},
+{path: 'flights', component:LoginComponent},
+]
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sk-planes/src/app/core/core.module.ts
+++ sk-planes/src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [DashboardComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule, MaterialModule
   ],
   exports: [DashboardComponent, LoginComponent]
 })
